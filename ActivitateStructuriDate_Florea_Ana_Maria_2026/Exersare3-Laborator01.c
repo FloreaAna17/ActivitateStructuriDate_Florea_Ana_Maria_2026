9ca8736bcaeb6eedf5462cc2e274766aeed555fa
Florea Ana Maria <anamaria200417@gmail.com>
2026-04-21 19:40:54 +0300

    Finalizat seminar01

--- ActivitateStructuriDate_Florea_Ana_Maria_2026/Exersare3-Laborator01.c
+++ ActivitateStructuriDate_Florea_Ana_Maria_2026/Exersare3-Laborator01.c
@@ -1,66 +1,66 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<malloc.h>
-
-struct Angajat {
-	int id; 
-	char* nume;
-	char* departament;
-	float salariu;
-};
-
-//initializare
-struct Angajat initializare(int id, const char* nume, const char* departament, float salariu) {
-	struct Angajat a;
-	a.id = id;
-	a.salariu = salariu;
-
-	a.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
-	strcpy_s(a.nume, strlen(nume) + 1, nume);
-	a.departament = (char*)malloc(sizeof(char) * (strlen(departament) + 1));
-	strcpy_s(a.departament, strlen(departament) + 1, departament);
-
-	return a;
-}
-
-//afisare
-void afisare(struct Angajat a) {
-	printf("%d. Angajatul %s din departamentul %s are salariul %.2f", a.id, a.nume, a.departament, a.salariu);
-	printf("\n--------------------------------\n");
-}
-
-//functie de transferare in alt departament
-void transferaDepartament(struct Angajat* a,const char* departamentNou) {
-	if (a->departament != NULL) {
-		free(a->departament);
-	}
-	a->departament= (char*)malloc(sizeof(char) * (strlen(departamentNou) + 1));
-	strcpy_s(a->departament, strlen(departamentNou) + 1, departamentNou);
-}
-
-//dezalocare
-void dezalocare(struct Angajat* a) {
-	
-	if (a->nume != NULL) {
-		free(a->nume);
-		a->nume = NULL;
-	}
-	if (a->departament != NULL) {
-		free(a->departament);
-		a->departament = NULL;
-	}
-}
-
-int main() {
-	struct Angajat a;
-	a = initializare(1, "Ana", "IT", 3200.5);
-	afisare(a);
-
-	transferaDepartament(&a, "MKT");
-	afisare(a);
-
-	dezalocare(&a);
-	afisare(a);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<malloc.h>
+//
+//struct Angajat {
+//	int id; 
+//	char* nume;
+//	char* departament;
+//	float salariu;
+//};
+//
+////initializare
+//struct Angajat initializare(int id, const char* nume, const char* departament, float salariu) {
+//	struct Angajat a;
+//	a.id = id;
+//	a.salariu = salariu;
+//
+//	a.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+//	strcpy_s(a.nume, strlen(nume) + 1, nume);
+//	a.departament = (char*)malloc(sizeof(char) * (strlen(departament) + 1));
+//	strcpy_s(a.departament, strlen(departament) + 1, departament);
+//
+//	return a;
+//}
+//
+////afisare
+//void afisare(struct Angajat a) {
+//	printf("%d. Angajatul %s din departamentul %s are salariul %.2f", a.id, a.nume, a.departament, a.salariu);
+//	printf("\n--------------------------------\n");
+//}
+//
+////functie de transferare in alt departament
+//void transferaDepartament(struct Angajat* a,const char* departamentNou) {
+//	if (a->departament != NULL) {
+//		free(a->departament);
+//	}
+//	a->departament= (char*)malloc(sizeof(char) * (strlen(departamentNou) + 1));
+//	strcpy_s(a->departament, strlen(departamentNou) + 1, departamentNou);
+//}
+//
+////dezalocare
+//void dezalocare(struct Angajat* a) {
+//	
+//	if (a->nume != NULL) {
+//		free(a->nume);
+//		a->nume = NULL;
+//	}
+//	if (a->departament != NULL) {
+//		free(a->departament);
+//		a->departament = NULL;
+//	}
+//}
+//
+//int main() {
+//	struct Angajat a;
+//	a = initializare(1, "Ana", "IT", 3200.5);
+//	afisare(a);
+//
+//	transferaDepartament(&a, "MKT");
+//	afisare(a);
+//
+//	dezalocare(&a);
+//	afisare(a);
+//	return 0;
+//}
